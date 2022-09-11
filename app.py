27a3d7108796bd779dcbd1b2b6fc97dbc9be6005
eben2840 <ebenmills200@gmail.com>
2022-09-11 23:04:47 +0000

    form added

--- app.py
+++ app.py
@@ -14,8 +14,10 @@ migrate= Migrate(app, db)
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
+    comment = db.Column(db.String(), nullable=True)
+    
     def __repr__(self):
-        return f"Course('{self.id}', {self.id}', )"
+        return f"Course('{self.id}', {self.name}', {self.comment}')"
 
 
 @app.route('/',methods=['GET','POST'])
@@ -61,7 +63,7 @@ def outteam():
         return render_template('debit.html')
     return render_template('debit.html')
 
-    
+'''  
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegistrationForm()
@@ -75,15 +77,14 @@ def signup():
         # Handle POST Request here
         return redirect(url_for('six'))
     return render_template('signup.html', form=form)
-
-        
+'''
+''''
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     name=Course.query.get_or_404(id)
     form = RegistrationForm()
     form.username.data=name.name
     if request.method=='POST':
-        
         print(form.username.data)
         print(form.password.data)
         namelogin=Course(name=form.username.data)
@@ -92,17 +93,42 @@ def update(id):
         # Handle POST Request here
         return redirect(url_for('six'))
     return render_template('signup.html', form=form)
+'''
+@app.route('/comment')
+def comment():
+    persons=Course.query.order_by(Course.id.desc()).all()
+    print(persons)
+    return render_template("comment.html", persons=persons)
+ 
 
-    
-@app.route('/hom')
+@app.route('/hom', methods=['POST','GET'])
 def hom():
-    names=Course.query.all()
-    print(names)
-    return render_template('venue.html', names=names)
+    form = RegistrationForm()
+    if request.method=='POST':
+        print(form.name.data)
+        print(form.comment.data)
+        newentry=Course(name=form.name.data, comment=form.comment.data)
+        db.session.add(newentry)
+        db.session.commit()
+        print("successful")
+        return redirect("/comment")
+    return render_template("venue.html", form=form)
 
+@app.route("/delete/<int:id>")
+def delete(id):
+    delete=Course.query.get_or_404(id)
+    try:
+            db.session.delete(delete)
+            db.session.commit()
+            return redirect('/comment') 
+    except: 
+        return "errrrrorrr"
+ 
+ 
 @app.route('/hotel')
 def hotel():
     return render_template('hotel.html')
+
 
 @app.route('/rest')
 def rest():
@@ -111,4 +137,4 @@ def rest():
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
-    app.run(port=50000,debug=True)
+    app.run(port=5000,debug=True)
--- forms.py
+++ forms.py
@@ -1,14 +1,10 @@
 
 from flask_wtf import FlaskForm
-
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, IntegerField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError, Regexp
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Name')
-    password = StringField('Phone', validators=[DataRequired()], description='Yes', render_kw={"placeholder": "Phone"})
-    email =  StringField('email', validators=[DataRequired()], render_kw={"placeholder": "Email"})
-    department = SelectField('Department', choices=[('IT','IT')])
-    identification = StringField('identification',  validators=[DataRequired()], render_kw={"placeholder": "Identification"})
-    submit = SubmitField('Sign Up')
+    name = StringField('name', validators=[DataRequired()])
+    comment =  StringField('comment', validators=[DataRequired()])
+    
